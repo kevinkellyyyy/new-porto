9e9f518a38ce65f6ddda8a745ccef600286ef467
Kevin Isyanta <kevinisyanta@Kevins-MacBook-Air.local>
2025-09-23 16:01:07 +0700

    minor linter

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-import React from 'react';
+import React from "react";
 // import ReactDOM from 'react-dom';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 
 // ReactDOM.render(
 //   <React.StrictMode>
@@ -19,7 +19,7 @@ root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-)
+);
 
 // Register Service Worker for PWA
 serviceWorkerRegistration.register();
